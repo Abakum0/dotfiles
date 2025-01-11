1699f0adf7abefd43539ed81d662845cdda36ee0
Abakum0 <Abakum0@users.noreply.github.com>
2025-01-11 06:07:16 +0100

    nvim: added completions

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,12 +1,14 @@
 return {
 	{
 		"williamboman/mason.nvim",
+		lazy = false,
 		config = function()
 			require("mason").setup()
 		end
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
 		config = function()
 			require("mason-lspconfig").setup({
 				ensure_installed = { "lua_ls", "jdtls" },
@@ -15,11 +17,17 @@ return {
 	},
 	{
 		"neovim/nvim-lspconfig",
+		lazy = false,
 		config = function()
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({})
-			lspconfig.jdtls.setup({})
+			local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities
+			})
+			lspconfig.jdtls.setup({
+				capabilities = capabilities
+			})
 			vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
 			vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
 			vim.keymap.set({'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
