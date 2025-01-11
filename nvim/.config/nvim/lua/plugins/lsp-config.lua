faa7155514fec9f50259242fd34de560770e6d82
Abakum0 <Abakum0@users.noreply.github.com>
2025-01-11 16:52:41 +0100

    nvim: lsp: removed explicit lazy loading config

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,14 +1,12 @@
 return {
 	{
 		"williamboman/mason.nvim",
-		lazy = false,
 		config = function()
 			require("mason").setup()
 		end
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
 		config = function()
 			require("mason-lspconfig").setup({
 				ensure_installed = { "lua_ls", "jdtls" },
@@ -17,7 +15,6 @@ return {
 	},
 	{
 		"neovim/nvim-lspconfig",
-		lazy = false,
 		config = function()
 			local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
