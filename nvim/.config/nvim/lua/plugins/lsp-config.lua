9b0981b4c79b79c4635a6ff1a09927c645ecf067
Abakum0 <Abakum0@users.noreply.github.com>
2025-01-31 15:10:00 +0100

    nvim: changed keymaps

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -26,9 +26,11 @@ return {
 				capabilities = capabilities
 			})
 			vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-			vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-			vim.keymap.set({'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
-
+			vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+			vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+            vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+            vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, {})
 		end
 	}
 }
